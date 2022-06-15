16a4974125fb2cd47597be7cc30085fa39d0603c
RafaPalau <rafaeldonizetip@gmai.com>
2022-06-14 23:03:45 -0300

    finish project

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,87 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("Ueba, eu sou um contrato e eu sou inteligente");
+     uint256 private seed;
+
+     event NewWave(address indexed from, uint256 timestamp, string music);
+
+      struct Wave {
+        address waver; 
+        string music; 
+        uint256 timestamp; 
     }
 
-    function wave() public {
+    Wave[] waves;
+
+     mapping(address => uint256) public lastWavedAt;
+
+   constructor() payable {
+       console.log("Contrato construido!");
+        /*
+         * Define a semente inicial
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _music) public {
+        /*
+         * Precisamos garantir que o valor corrente de timestamp é ao menos 15 minutos maior que o último timestamp armazenado
+         */
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Espere 30 segundos"
+        );
+
+        /*
+         * Atualiza o timestamp atual do usuário
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
-        console.log("%s deu tchauzinho!", msg.sender);
+        console.log("%s Enviou a musica %s", msg.sender, _music);
+
+        waves.push(Wave(msg.sender, _music, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("# randomico gerado: %d", seed);
+
+        
+
+       if (seed <= 50) {
+            console.log("%s ganhou!", msg.sender);
+
+            /*
+             * O mesmo código que tínhamos anteriormente para enviar o prêmio.
+             */
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Tentando sacar mais dinheiro que o contrato possui."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Falhou em sacar dinheiro do contrato.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _music);
+    }
+
+  
+
+
+
+    /*
+     * Adicionei uma função getAllWaves que retornará as músicas.
+     * Isso permitirá recuperar as músicas a partir do nosso site!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("Temos um total de %d tchauzinhos!", totalWaves);
+        // Opcional: Adicione esta linha se você quer ver o contrato imprimir o valor!
+        // Também imprimirá em run.js.
+        console.log("Temos %d tchauzinhos no total!", totalWaves);
         return totalWaves;
     }
 }
