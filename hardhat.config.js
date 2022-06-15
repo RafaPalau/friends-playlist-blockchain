d6e7bb96c1c4735df59a77a59023ca60ddebeb09
RafaPalau <rafaeldonizetip@gmai.com>
2022-06-14 21:23:55 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_KEY,
+      accounts: [process.env.METAMASK_KEY],
+    },
+  },
+};
+
